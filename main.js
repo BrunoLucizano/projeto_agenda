3e7a9be3f9605d48fc97a2319993086a8b3a4f1d
BrunoLucizano <brunolucizano@gmail.com>
2023-07-29 23:57:11 -0300

    adicionando funcionalidade "tipo"

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('form-contato');
 const imgCelular = '<img src="./images/celular.png" alt="celular" />';
-const imgfixo = '<img src="./images/fixo.png" alt="telefone fixo" />';
+const imgFixo = '<img src="./images/fixo.png" alt="telefone fixo" />';
 const nomes = [];
 const numeros = [];
 
@@ -12,7 +12,6 @@ form.addEventListener('submit', function(e) {
     adicionaLinha();
 
     atualizaTabela();
-
 });
 
 function adicionaLinha() {
@@ -20,16 +19,17 @@ function adicionaLinha() {
     const inputTelefone = document.getElementById('telefone');
 
     if (nomes.includes(inputNomeContato.value)) {
-        alert(`O contato: ${inputNomeContato.value} já foi inserido`)
-    }
-
-    else{
+        alert(`O contato: ${inputNomeContato.value} já foi inserido`);
+    } else {
         nomes.push(inputNomeContato.value);
-        numeros.push(parseInt(inputTelefone.value));
+        numeros.push(inputTelefone.value);
+
+        let tipoTelefone = inputTelefone.value.length === 11 ? 'celular' : 'fixo';
 
         let linha = '<tr>';
         linha += `<td>${inputNomeContato.value}</td>`;
         linha += `<td>${inputTelefone.value}</td>`;
+        linha += `<td>${getImagemTipoTelefone(tipoTelefone)}</td>`;
         linha += '</tr>';
 
         linhas += linha;
@@ -42,4 +42,8 @@ function adicionaLinha() {
 function atualizaTabela() {
     const corpoTabela = document.querySelector('tbody');
     corpoTabela.innerHTML = linhas;
+}
+
+function getImagemTipoTelefone(tipo) {
+    return tipo === 'celular' ? imgCelular : imgFixo;
 }
